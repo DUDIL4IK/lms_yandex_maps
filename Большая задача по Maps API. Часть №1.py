6f45ec7c7a2a01516989c7c8cfec42c72f82bb30
DUDIL4IK <150362152+DUDIL4IK@users.noreply.github.com>
2024-02-06 18:19:41 +0500

    Update Большая задача по Maps API. Часть №1.py

--- Большая задача по Maps API. Часть №1.py
+++ Большая задача по Maps API. Часть №1.py
@@ -12,10 +12,9 @@ class MapApp:
         print('Введите координаты через пробел:')
         self.cor = input()
         if ',' in self.cor:
-            print('Координаты введены неверно')
-        print('Введите масштаб в %:')
-        print('Чем меньше число, тем больше охват карты')
-        self.mash = input()
+            print('Используйте точку, а не запятую!')
+        print('Введите масштаб(от 1 до 17):')
+        self.mash = int(input())
         self.check()
         self.getImage()
         self.initUI()
@@ -24,33 +23,29 @@ class MapApp:
         self.cor = ','.join(self.cor.split())
         if 100 < float(self.mash):
             self.prov = 1
-            print('Маштаб должен быть введен в диапазоне от 0 до 100!')
-        self.mash = round(0.17 * int(self.mash))
+            print('Маштаб должен быть введен в диапазоне от 0 до 17!')
         if self.mash < 0:
             self.mash = 0
         if self.mash > 17:
             self.mash = 17
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=" + self.cor + "&z=" + str(self.mash) + "&size=600,450&l=map"
-        response = requests.get(map_request)
+        map_req = "http://static-maps.yandex.ru/1.x/?ll=" + self.cor + "&z=" + str(self.mash) + "&size=600,450&l=map"
+        response = requests.get(map_req)
 
         if not response:
             print('Координаты введены неверно!')
             print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
+        self.map_im = "map.png"
+        with open(self.map_im, "wb") as file:
             file.write(response.content)
 
     def initUI(self):
         self.screen = pygame.display.set_mode(SCREEN_SIZE)
-        pygame.display.set_caption('Отображение карты')
-
-        self.map_image = pygame.image.load(self.map_file)
+        pygame.display.set_caption('Большая задача по Maps API. Часть №1')
+        self.map_image = pygame.image.load(self.map_im)
         self.screen.blit(self.map_image, (0, 0))
         pygame.display.flip()
 
@@ -62,7 +57,7 @@ class MapApp:
                     running = False
 
         pygame.quit()
-        os.remove(self.map_file)
+        os.remove(self.map_im)
         sys.exit()
 
 if __name__ == '__main__':
